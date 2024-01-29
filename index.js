ba8b07be2b0d7a71dd72123851a251b55193f69c
NareshBiradar1 <nareshdb555@gmail.com>
2024-01-30 02:46:16 +0530

    possible moves for pawn

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
+function is2DArrayContains1DArray(arr2D, arr1D) {
+    for (var i = 0; i < arr2D.length; i++) {
+      var currentRow = arr2D[i];
+  
+      // Check if the length of the current row matches the length of the target 1D array
+      if (currentRow.length === arr1D.length) {
+        // Check if each element in the current row is equal to the corresponding element in the target 1D array
+        var match = true;
+        for (var j = 0; j < currentRow.length; j++) {
+          if (currentRow[j] !== arr1D[j]) {
+            match = false;
+            break; // exit the loop early if a mismatch is found
+          }
+        }
+  
+        // If all elements match, return true
+        if (match) {
+          return true;
+        }
+      }
+    }
+  
+    // If no match is found, return false
+    return false;
+  }
+
+  function showError(data){
+    heading.innerHTML=data;
+
+    setTimeout(function() {
+        heading.innerHTML = '';
+    }, 1000);
+  }
+
 let allBoxes = document.querySelectorAll(".chessBox");
 
 let container = document.querySelector(".container");
+
+let heading = document.querySelector(".heading")
 
 let count=0;
 let addBlackClass=true;
@@ -42,15 +78,71 @@ container.addEventListener('click',function(e){
     }
     else{
         swap=!swap;
+        let prevBox;
         for(let i=0;i<allBoxes.length;i++){
             if(allBoxes[i].classList.contains("active")){
-                let childIcon = (allBoxes[i].childNodes[0]);
-                allBoxes[i].removeChild(allBoxes[i].childNodes[0]);
-                e.target.appendChild(childIcon);
+                prevBox =allBoxes[i];
                 allBoxes[i].classList.remove("active");
             }
         }
+        let curPos = [Number(e.target.classList[1]) , Number(e.target.classList[2])];
+        let prevPos = [Number(prevBox.classList[1]) , Number(prevBox.classList[2])];
+
+        if(curPos[0]>10) curPos[0]=curPos[0]%10;
+        if(curPos[1]>10) curPos[1]=curPos[1]%10;
+        if(prevPos[0]>10) prevPos[0]=prevPos[0]%10;
+        if(prevPos[1]>10) prevPos[1]=prevPos[1]%10;
+
+        console.log(prevPos,curPos);
+        let post = prevBox.classList[4];
+        let colour = prevBox.classList[3];
         
+        console.log(post,colour);
+        let possibleMoves;
+        
+        if(post=="soldier"){
+            if(prevPos[0]=='2' || colour=="white"){
+                possibleMoves = [[prevPos[0]+1,prevPos[1]],[prevPos[0]+2,prevPos[1]]];
+                console.log(possibleMoves);
+            }
+            else if(prevPos[0]=='7' || colour=="black"){
+                possibleMoves = [[prevPos[0]-1,prevPos[1]],[prevPos[0]-2,prevPos[1]]];
+                console.log(possibleMoves);
+            }
+            else if(colour=="white"){
+                possibleMoves = [[prevPos[0]+1,prevPos[1]]];
+                console.log(possibleMoves);
+            }
+            else{
+                possibleMoves = [[prevPos[0]-1,prevPos[1]]];
+                console.log(possibleMoves);
+            }
+            // if(prevPos)
+
+            if(is2DArrayContains1DArray(possibleMoves,curPos)){
+                let childIcon=prevBox.childNodes[0];
+                prevBox.removeChild(prevBox.childNodes[0]);
+                e.target.appendChild(childIcon);
+            }
+            else{
+                showError("Invalid Move");
+            }
+        }
+        else if(post=="elephant"){
+
+        }
+        else if(post=="horse"){
+            
+        }
+        else if(post=="camel"){
+            
+        }
+        else if(post=="queen"){
+            
+        }
+        else if(post=="king"){
+            
+        }
     }
 
 
